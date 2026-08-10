eef8f63f25889b949f471004d826f94ea0cbf66f
Shivam lodhi <152368473+Shivam7570@users.noreply.github.com>
2026-08-10 14:45:56 +0530

    Update CORS origin to production URL

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://www.echothejungle.com/",
     credentials: true,
   })
 );
